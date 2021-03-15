a92bf486cddf936d4afd2c8d0e11e2280862494d
Antoine GORON <antoine.goron@etu.imt-lille-douai.fr>
2021-03-15 16:12:43 +0100

    cookies

--- TP3/Exo 1/index.php
+++ TP3/Exo 1/index.php
@@ -2,18 +2,36 @@
     <center> <h1 class="titre">Bienvenue sur la page pro d'Antoine Goron</h1> </center>
 </header>
 
+<center>
+<form id="style_form" action="index.php" method="GET">
+    <select name="habillage.css">
+        <option value="style1">Bleu Gris sympa</option>
+        <option value="style2">Le prochain est à voir</option>
+    </select>
+    <input type="submit" value="Appliquer" />
+</form>
+</center>
+
 <?php
+
     require_once("teamplate_header.php");
     require_once("teamplate_menu.php");
     $currentPageId = 'accueil';
     $currentLangId = 'fr';
+    $currentStyle = "style1";
     if(isset($_GET['page'])) {
         $currentPageId = $_GET['page'];
     }
     if(isset($_GET['lang'])) {
         $currentLangId = $_GET['lang'];
     }
+    if(isset($_GET['habillage.css'])) {
+        $currentLangId = $_GET['habillage.css'];
+    }
     renderMenuToHTML($currentPageId, $currentLangId);
+    
+    setcookie("style", $currentStyle, time()+3600);
+
 ?>
 <section class="corps">
 
