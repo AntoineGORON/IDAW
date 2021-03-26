cbc5226bf728d9d81934878abf7118c3177b5b70
Antoine GORON <antoine.goron@etu.imt-lille-douai.fr>
2021-03-26 11:59:31 +0100

    TP4 exo 1

--- TP4/Exo 3/fr/contact.php
+++
@@ -1,9 +0,0 @@
-
-        <div class="conteneur-flexible ligne ">
-            <div class="element-flexible bleu-clair element-hw-autres"> <center> <h2>Contactez-moi<h2> 
-            <h3> Facebook </h3>   
-            <p>Antoine Goron, petite photo de profil Poudl'art</p>
-            <h3>E-Mail</h3>
-            <p>goron.antoine@gmail.com</p> </center>
-            </div>
-        </div>        
